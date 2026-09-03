a4a9e152c02f0f8bade4f039353f4ce7b33c4e6f
KaryMonge <karinadurazno21@gmail.com>
2026-09-02 21:31:24 -0600

    Configura proxy HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
 
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
 
         $middleware->alias([
             'supervisor' => \App\Http\Middleware\SupervisorMiddleware::class,

951689f0358be4897e2ebe601047726556bd6f39
KaryMonge <karinadurazno21@gmail.com>
2026-09-09 20:15:50 -0600

    Consolidar migraciones y actualizar estructura de base de datos

--- database/migrations/2026_05_14_024906_create_areas_table.php
+++ database/migrations/2026_05_14_024906_create_areas_table.php
@@ -6,23 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-    Schema::create('areas', function (Blueprint $table) {
-    $table->id();
-    $table->string('nombre')->unique();
-    $table->string('descripcion')->nullable();
-    $table->boolean('activo')->default(true);
-    $table->timestamps();
-});
+        Schema::create('areas', function (Blueprint $table) {
+            $table->id('id_areas');
+            $table->string('nombre')->unique();
+            $table->string('descripcion')->nullable();
+            $table->boolean('activo')->default(true);
+            $table->timestamps();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('areas');
